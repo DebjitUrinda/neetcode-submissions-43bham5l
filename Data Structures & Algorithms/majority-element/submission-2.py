03660e330be1144e0e5e599ac62884ea715974a1
d.j. <87750461+DebjitUrinda@users.noreply.github.com>
2026-04-09 12:33:47 +0530

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 1
+        elem = nums[0]
+
+        for i in range(1,len(nums)):
+            print("loop start:", count)
+            if nums[i] == elem:
+                count += 1
+
+            elif nums[i] != elem:
+                count -= 1
+                if count==0:
+                    elem = nums[i]
+
+            print("loop ends:", count)
+
+        return elem
